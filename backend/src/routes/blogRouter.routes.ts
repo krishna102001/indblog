76906d5e400fd49970b777d82c067bc2eafbc629
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-30 23:50:31 +0530

    middleware is created

--- backend/src/routes/blogRouter.routes.ts
+++ backend/src/routes/blogRouter.routes.ts
@@ -1,10 +1,29 @@
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
 
 const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
   };
 }>();
+
+//middleware is created
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = (await c.req.header("Authorization")) || "";
+  const token = authHeader?.split(" ")[1];
+  const user = await verify(token, c.env.JWT_SECRET);
+  console.log(user);
+  if (user && typeof user.id == "string") {
+    c.set("userId", user.id);
+    await next();
+  } else {
+    return c.json({ success: false, message: "unauthorized access" }, 401);
+  }
+});
 
 blogRouter.post("/api/v1/blog", (c) => {
   return c.text("added blog");
