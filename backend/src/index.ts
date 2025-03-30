7fd5d3f50ec902780f9f56769ef6c7607b3054a4
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-30 22:16:04 +0530

    removed comment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,13 +5,6 @@ import blogRouter from "./routes/blogRouter.routes";
 
 const app = new Hono();
 
-// POST /api/v1/user/signup
-// POST /api/v1/user/signin
-// POST /api/v1/blog
-// PUT /api/v1/blog
-// GET /api/v1/blog/:id
-// GET /api/v1/blog/bulk
-
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
