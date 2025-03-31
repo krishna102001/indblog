76b6857cfdeb97977a353d88cdc1492b72f56ec4
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-31 17:54:49 +0530

    removed import css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <div className='bg-red-500'>Hello From Ind Blog Platform</div>
+      <div className='bg-red-500'>Hello from Ind Blog</div>
     </>
   );
 }
