fee8631e86e370140b2e63baa34ed154ca857154
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-31 21:18:21 +0530

    fix:styling

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar />
-      <div className='flex-1 py-10'>{children}</div>
+      <div className='flex-1 py-4'>{children}</div>
       <Footer />
     </div>
   );
