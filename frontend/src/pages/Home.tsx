64bb80235c8327f3fdacf577b1330245fd98bda5
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-31 21:18:37 +0530

    homepage done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,28 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-3'>
+      <div className='m-10 mt-20 sm:ml-32 sm:mt-32 col-span-2 '>
+        <h1 className='text-6xl sm:text-8xl'>
+          Human <br />
+          Stories & ideas
+        </h1>
+        <p className='mt-8 text-xl sm:text-2xl font-light'>
+          A perfect place to read, write and share your thoughts
+        </p>
+        <p className='bg-gray-900 rounded-full text-[#ff7002] max-w-max p-2 px-6 mt-7 hover:shadow-md hover:shadow-orange-500 hover:text-[#f59347]'>
+          Start Writing
+        </p>
+      </div>
+
+      <div className='ml-auto hidden sm:block'>
+        <img
+          src='homo-img.webp'
+          alt='home-picture'
+          className='w-md h-[600px]'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
