de9da640a66aa78d0fa679f4d020fed3e5cb382d
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-31 19:11:53 +0530

    fix:styling:

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,11 @@ const Navbar = () => {
   return (
     <div className='flex justify-between p-4 sm:px-10 bg-gray-200 shadow-md shadow-gray-300'>
       <div className='flex items-center'>
-        <img src='ib.png' alt='ind-blog-logo' className='w-10 h-10' />
+        <img
+          src='ib.png'
+          alt='ind-blog-logo'
+          className='sm:w-10 sm:h-10 w-6 h-6'
+        />
         <Link to='/'>
           <span className='text-lg sm:text-3xl font-extrabold tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-[#ff1600] to-[#ff7002]'>
             Ind Blog
@@ -13,15 +17,15 @@ const Navbar = () => {
       </div>
       <div className='flex items-center sm:gap-4 gap-2'>
         <Link to='/'>
-          <p className='cursor-pointer'>Home</p>
+          <p className='cursor-pointer text-lg'>Home</p>
         </Link>
         <Link to='/signin'>
-          <p className='cursor-pointer bg-[#ff4f01] p-2 rounded-lg text-white sm:tracking-wider shadow-sm shadow-blue-700 hover:shadow-blue-800 hover:shadow-md'>
+          <p className='cursor-pointer bg-[#ff4f01] p-1 px-2 rounded-lg text-white sm:tracking-wider shadow-sm shadow-blue-700 hover:shadow-blue-800 hover:shadow-md'>
             Sign in
           </p>
         </Link>
         <Link to='/signup'>
-          <p className='cursor-pointer bg-blue-600 p-2 rounded-lg text-white sm:tracking-wider shadow-sm shadow-blue-700 hover:shadow-[#ff4f01] hover:shadow-md'>
+          <p className='cursor-pointer bg-blue-600 p-1 px-2 rounded-lg text-white sm:tracking-wider shadow-sm shadow-blue-700 hover:shadow-[#ff4f01] hover:shadow-md'>
             Register
           </p>
         </Link>
