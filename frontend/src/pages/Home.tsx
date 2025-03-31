3b0bbbdea98295b9e1290cff4af594799dabf148
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-31 21:34:34 +0530

    added linear gradient border

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,12 @@ const Home = () => {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-3'>
       <div className='m-10 mt-20 sm:ml-32 sm:mt-32 col-span-2 '>
+        <div className='relative inline-block p-[2px] text-sm ml-2 rounded-full bg-gradient-to-r from-blue-500 to-purple-500'>
+          <p className='bg-gray-200 rounded-full px-2 py-1 font-extralight text-black'>
+            AI Enhanced Summary
+          </p>
+        </div>
+
         <h1 className='text-6xl sm:text-8xl'>
           Human <br />
           Stories & ideas
