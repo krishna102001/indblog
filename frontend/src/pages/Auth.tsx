801227ad118249dafb56be16029e9a0097896e1c
krishna102001 <krishnakantmaurya@icloud.com>
2025-04-01 01:15:51 +0530

    separeted a component

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,41 +1,24 @@
 import { useState } from "react";
-import LabelledInput from "../components/LabelledInput";
-import { signinType } from "@krishnakantmaurya/indblog-common";
 import AuthHeader from "../components/AuthHeader";
+import Signin from "../components/Signin";
+import Signup from "../components/Signup";
 
 const Auth = () => {
-  const [signInInput, setSignInInput] = useState<signinType>({
-    email: "",
-    password: "",
-  });
+  const [isRegister, setIsRegister] = useState(false);
   return (
     <div className='flex flex-col items-center justify-center min-h-[70vh]'>
       <div className='flex flex-col items-center'>
-        {/* {JSON.stringify(signInInput)} */}
-        <div className='bg-white rounded-md drop-shadow-lg shadow-lg p-10 px-16'>
-          <AuthHeader />
-          <div className='mt-4'>
-            <LabelledInput
-              label='Email'
-              type='email'
-              placeholder='indblog@gmail.com'
-              onChange={(e) =>
-                setSignInInput({ ...signInInput, email: e.target.value })
-              }
-            />
-            <LabelledInput
-              label='Password'
-              placeholder='krishna12'
-              onChange={(e) =>
-                setSignInInput({ ...signInInput, password: e.target.value })
-              }
-            />
-          </div>
-          <button className='text-gray-100 bg-gradient-to-r from-[#ff1600] to-[#ff7002] p-2 px-4 rounded-full shadow-md hover:shadow-[#ff6900] mt-1'>
-            Login
-          </button>
-          <div className='flex font-extralight justify-end text-xs mt-4'>
-            <p className='mr-1 text-blue-600 underline'>Create account now</p>
+        <div className='bg-white rounded-md drop-shadow-lg shadow-lg p-10 px-16 min-w-96'>
+          <AuthHeader isRegister={isRegister} />
+          <div className='mt-4'>{isRegister ? <Signup /> : <Signin />}</div>
+
+          <div className='flex font-extralight justify-end text-sm mt-4 cursor-pointer'>
+            <p
+              className='mr-1 text-blue-600 underline'
+              onClick={() => setIsRegister(!isRegister)}
+            >
+              {isRegister ? "Already have account ?" : "Create account now"}
+            </p>
           </div>
         </div>
       </div>
