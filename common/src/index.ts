c79422c74ecffa2c255068be8ec99a609d59ed69
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-31 15:02:55 +0530

    zod validation written

---
+++ common/src/index.ts
@@ -0,0 +1,27 @@
+import { ParseStatus, string, z } from "zod";
+
+export const signupInput = z.object({
+  name: string(),
+  email: string().email(),
+  password: string().min(6).max(20),
+});
+
+export const signinInput = z.object({
+  email: string().email(),
+  password: string(),
+});
+
+export const blogCreateInput = z.object({
+  title: string(),
+  content: string(),
+});
+
+export const blogUpdateInput = z.object({
+  title: string().optional(),
+  content: string().optional(),
+});
+
+export type signupInput = z.infer<typeof signupInput>;
+export type signinType = z.infer<typeof signinInput>;
+export type blogCreateType = z.infer<typeof blogCreateInput>;
+export type blogUpdateType = z.infer<typeof blogUpdateInput>;
