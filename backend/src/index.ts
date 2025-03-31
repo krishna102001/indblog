ec499806b499393df4c187c28138051919fbaae0
krishna102001 <krishnakantmaurya@icloud.com>
2025-04-01 01:10:56 +0530

    cors introduced

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,10 @@ import { Hono } from "hono";
 
 import userRouter from "./routes/userRouter.routes";
 import blogRouter from "./routes/blogRouter.routes";
+import { cors } from "hono/cors";
 
 const app = new Hono();
-
+app.use("/*", cors());
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/", blogRouter);
 
