4e7a806e5177f6258d35e6f3524f62bae6bd51ee
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-31 16:38:44 +0530

    tailwind setup done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>Hello From Ind Blog Platform</div>
+      <div className='bg-red-500'>Hello From Ind Blog Platform</div>
     </>
   );
 }
