618a34cfe473eeb1b8478fd63bf5ca884675a28e
krishna102001 <krishnakantmaurya@icloud.com>
2025-04-01 16:43:48 +0530

    context package made

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ToastContainer } from "react-toastify";
+import { AppContextProvider } from "./context/AppContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToastContainer />
-    <App />
+    <AppContextProvider>
+      <ToastContainer />
+      <App />
+    </AppContextProvider>
   </StrictMode>
 );
