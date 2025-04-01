2eff52955c2cd3a4e0de70925d7cbf89c174f4ae
krishna102001 <krishnakantmaurya@icloud.com>
2025-04-01 16:43:26 +0530

    context package made

---
+++ frontend/src/context/AppContext.tsx
@@ -0,0 +1,39 @@
+import { jwtDecode } from "jwt-decode";
+import React, { createContext, useContext, useEffect, useState } from "react";
+interface UserData {
+  name: string;
+  email: string;
+}
+type AppContext = {
+  userData: UserData | undefined;
+  setUserData: (userData: UserData | undefined) => void;
+};
+
+const AppContext = createContext<AppContext | undefined>(undefined);
+
+export const AppContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [userData, setUserData] = useState<UserData>();
+  // console.log(userData);
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    if (token) {
+      const user: UserData = jwtDecode(token);
+      setUserData(user);
+    }
+  }, [token]);
+
+  return (
+    <AppContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  return context as AppContext;
+};
